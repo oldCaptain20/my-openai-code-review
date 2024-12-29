2670f7d279b9359a7f454a3faa413e80c311da8b
TubeStriking <guanzhu15211065912@163.com>
2024-12-29 18:16:38 +0800

    初始化工程

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,11 +1,9 @@
 package plus.gaga.middleware.sdk;
 
 
-import org.eclipse.jgit.api.GitCommand;
-
 public class OpenAiCodeReview {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
     }
 
