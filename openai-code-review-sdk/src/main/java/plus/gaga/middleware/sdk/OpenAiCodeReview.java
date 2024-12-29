46921cf9a07983c4abc8a7b8bfcd4bb789613f7b
TubeStriking <guanzhu15211065912@163.com>
2024-12-29 19:41:56 +0800

    初始化工程

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -4,7 +4,7 @@ package plus.gaga.middleware.sdk;
 public class OpenAiCodeReview {
 
     public static void main(String[] args) {
-        System.out.println("\"测试执行\" = " + "测试执行");
+        System.out.println("\"测试执行\" === " + "测试执行");
     }
 
 }
