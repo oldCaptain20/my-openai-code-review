7b81136e23c255f5f62ad9f30f6f73ebfd05a27d
zhangsan <zhangsan@163.com>
2024-12-31 17:33:12 +0800

    测试提交 2024年12月31日

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -5,6 +5,7 @@ public class OpenAiCodeReview {
 
     public static void main(String[] args) {
         System.out.println("\"测试执行\" === " + "测试执行");
+        System.out.println("\"2024年12月31日\" = " + "2024年12月31日");
     }
 
 }
