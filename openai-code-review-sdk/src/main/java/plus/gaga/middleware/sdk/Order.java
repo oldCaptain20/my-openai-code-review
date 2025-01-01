80572681aebe48260ee2d200d30a6311a6eab4ce
TubeStriking <guanzhu15211065912@163.com>
2025-01-01 15:18:30 +0800

    文件回退

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/Order.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/Order.java
@@ -3,7 +3,5 @@ package plus.gaga.middleware.sdk;
 public class Order {
     public static void main(String[] args) {
         System.out.println("\"开发了order模块\" = " + "开发了order模块");
-        System.out.println("\"开发了order模块\" = " + "开发了order模块");
-        System.out.println("\"开发了order模块\" = " + "开发了order模块");
     }
 }
