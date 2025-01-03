ebbb6795a39280816e4f247125f588cf7c5e3e31
zhangsan <zhangsan@163.com>
2025-01-03 15:42:11 +0800

    删除main中 不需要的东西

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -5,8 +5,6 @@ public class OpenAiCodeReview {
 
     public static void main(String[] args) {
         System.out.println("\"测试执行\" === " + "测试执行");
-        System.out.println("\"2024年12月31日\" = " + "2024年12月31日");
-        System.out.println("保留重置（keep）：重置 HEAD，但保留未提交的更改，适用于希望保留本地更改的场景。");
     }
 
 }
